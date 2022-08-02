71d010d96edb7076da77fca5c539dcb6ed8dc1cf
RinkiMaurya <rinkimaurya444@gmail.com>
2022-08-02 22:08:04 +0530

    news

--- src/App.js
+++ src/App.js
@@ -1,21 +1,3 @@
-//  import React from "react";
-// // import { Card, Button } from "antd";
-// // import axios from "axios";
-// import "./App.css";
-
-// // const { Meta } = Card;
-
-// function App() {
-//   async function ap() {
-//     let response = await fetch("https://newsapi.org/v2/everything?q=bitcoin&apiKey=a5f3ef64e5fc41cb8306d4aa8de665e4");
-//     let result = await response.json();
-
-//     console.log(result);
-//   }
-//   ap();
-//  return <h1>Hii</h1>
-// };
- 
 import React from "react"
 import Main from './Components/Main'
 import "./Components/style.css"
